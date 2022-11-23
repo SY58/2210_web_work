ea1124a1be26a3c65e924963810b95c9054f0da4
syCHOI <sychoi.me@gmail.com>
2022-11-23 11:03:00 +0900

    Step04 게시판 파일 업로드/다운로드 기능 추가

---
+++ Step04_Final/src/main/java/test/file/dao/FileDao.java
@@ -0,0 +1,193 @@
+package test.file.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import test.file.dto.FileDto;
+import test.util.DbcpBean;
+
+public class FileDao {
+	//static 필드
+	private static FileDao dao;
+	//외부에서 객체 생성하지 못하도록 생성자를 private로
+	private FileDao() {}
+	//자신의 참조값을 리턴해주는 메소드
+	public static FileDao getInstance() {
+		if(dao==null) {
+			dao=new FileDao();
+		}
+		return dao;
+	}
+	
+	//파일을 삭제하는 메소드
+	public boolean delete(int num) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int rowCount = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = "DELETE FROM board_file"
+					+ " WHERE num=?";
+			pstmt = conn.prepareStatement(sql);
+			//?에 바인딩
+			pstmt.setInt(1, num);
+			//INSERT or UPDATE or DELETE문을 수행하고 수정,삭제,추가된 row의 개수 리턴
+			rowCount = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e2) {
+			}
+		}
+
+		if (rowCount > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}	
+	
+	//파일 하나의 정보를 리턴하는 메소드	
+	public FileDto getData(int num) {		
+		FileDto dto=null;
+		//필요한 객체를 담을 지역변수를 미리 만들어둔다.
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			//Connection Pool에서 Connection 객체를 하나 얻어온다.
+			conn = new DbcpBean().getConn();
+			//실행할 sql문의 뼈대 구성하기
+			String sql = "SELECT writer, title, orgFileName, saveFileName, fileSize, regdate"
+					+ " From board_file"
+					+ " WHERE num=?";			
+			//sql문의 ?에 바인딩할게 있으면 한다.			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			//SELECT문을 수행하고 결과값을 받아온다.
+			rs = pstmt.executeQuery();
+			//반복문 돌면서 resultSet에서 필요한 값을 얻어낸다.
+			if (rs.next()) {
+				dto=new FileDto();				
+				dto.setNum(num);
+				dto.setWriter(rs.getString("writer"));
+				dto.setTitle(rs.getString("title"));
+				dto.setOrgFileName(rs.getString("orgFileName"));
+				dto.setSaveFileName(rs.getString("saveFileName"));
+				dto.setFileSize(rs.getLong("fileSize"));
+				dto.setRegdate(rs.getString("regdate"));				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close(); //Connection Pool에 Connection 반납하기
+
+			} catch (Exception e2) {
+			}
+		}
+		return dto;
+	}
+
+	//업로드된 파일 정보를 DB 에 저장하는 메소드	
+	public boolean insert(FileDto dto) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int rowCount = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = "INSERT INTO board_file"
+					+ " (num, writer, title, orgFileName, saveFileName, fileSize, regdate)"
+					+ " VALUES(board_file_seq.NEXTVAL, ?, ?, ?, ?, ?, SYSDATE)";
+			pstmt = conn.prepareStatement(sql);
+			//?에 바인딩
+			pstmt.setString(1, dto.getWriter());
+			pstmt.setString(2, dto.getTitle());
+			pstmt.setString(3, dto.getOrgFileName());
+			pstmt.setString(4, dto.getSaveFileName());
+			pstmt.setLong(5, dto.getFileSize());			
+			//INSERT or UPDATE or DELETE문을 수행하고 수정,삭제,추가된 row의 개수 리턴
+			rowCount = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e2) {
+			}
+		}
+
+		if (rowCount > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	//파일 목록을 리턴해주는 메소드	
+	public List<FileDto> getList() {
+		//파일 목록을 담을  ArrayList 객체 생성
+		List<FileDto> list=new ArrayList<FileDto>();
+		//필요한 객체를 담을 지역변수를 미리 만들어둔다.
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;		
+		try {
+			//Connection Pool에서 Connection 객체를 하나 얻어온다.
+			conn = new DbcpBean().getConn();
+			//실행할 sql문의 뼈대 구성하기
+			String sql = "SELECT num, writer, title, orgFileName, fileSize, TO_CHAR(regdate, 'YYYY.MM.DD HH24:MI') regdate"
+					+ " FROM board_file"
+					+ " ORDER BY num DESC";
+			//sql문의 ?에 바인딩할게 있으면 한다.
+
+			pstmt = conn.prepareStatement(sql);
+			//SELECT문을 수행하고 결과값을 받아온다.
+			rs = pstmt.executeQuery();
+			//반복문 돌면서 resultSet에서 필요한 값을 얻어낸다.
+			while (rs.next()) {
+				//FileDto 객체에 select된 row하나의 정보를 담고
+				FileDto dto=new FileDto();				
+				dto.setNum(rs.getInt("num"));
+				dto.setWriter(rs.getString("writer"));
+				dto.setTitle(rs.getString("title"));
+				dto.setOrgFileName(rs.getString("orgFileName"));								
+				dto.setFileSize(rs.getLong("fileSize"));
+				dto.setRegdate(rs.getString("regdate"));
+				//ArrayList 객체에 누적시킨다.
+				list.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close(); //Connection Pool에 Connection 반납하기
+
+			} catch (Exception e2) {
+			}
+		}
+		return list;
+	}
+
+}
