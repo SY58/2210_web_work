fe83f6dcd6b293074e45fce236a4b280bc0c8d5c
syCHOI <sychoi.me@gmail.com>
2022-11-24 15:26:45 +0900

    Step04 에 프로필 이미지 등록, 수정 기능 구현

--- Step04_Final/src/main/java/test/users/dao/UsersDao.java
+++ Step04_Final/src/main/java/test/users/dao/UsersDao.java
@@ -60,12 +60,13 @@ public class UsersDao {
 		try {
 			conn = new DbcpBean().getConn();
 			String sql = "UPDATE users"
-					+ " SET email=?"
+					+ " SET email=?, profile=?"
 					+ " WHERE id=?";
 			pstmt = conn.prepareStatement(sql);
 			//?에 바인딩
 			pstmt.setString(1, dto.getEmail());
-			pstmt.setString(2, dto.getId());
+			pstmt.setString(2, dto.getProfile());
+			pstmt.setString(3, dto.getId());
 			//INSERT or UPDATE or DELETE문을 수행하고 수정,삭제,추가된 row의 개수 리턴
 			rowCount = pstmt.executeUpdate();
 		} catch (Exception e) {
